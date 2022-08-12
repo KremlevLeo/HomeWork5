84abc61ed81cc220f7b2c6d8e69cb2c0494ed317
Леонид Кремлев <62460369+KremlevLeo@users.noreply.github.com>
2022-08-12 15:21:51 +0500

    Task 3 Done

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76*/
+
+Console.Write("Enter count of numberd for array: ");
+int countNumbers = int.Parse(Console.ReadLine());
+int[] arr = FillArray(countNumbers);
+Console.Write(Max(arr) - Min(arr));
+
+int[] FillArray(int a)
+{
+    Console.Write("[ ");
+    int[] arr = new int[a];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 101);
+        Console.Write(arr[i] + " ");
+    }
+    Console.Write("] -> ");
+    return arr;
+}
+
+int Min(int[] array)
+{
+    int min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (min > array[i])
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+int Max(int[] array)
+{
+    int max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (max < array[i])
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
