73ca9e93bb345879d5dd4fbed3b28bbc752311be
Леонид Кремлев <62460369+KremlevLeo@users.noreply.github.com>
2022-08-12 14:50:19 +0500

    Task 1 Done

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2     */
+
+
+Console.Write("Enter count of numberd for array: ");
+int countNumbers = int.Parse(Console.ReadLine());
+int[] arr = FillArray(countNumbers);
+
+Console.Write(ParytiCheck(arr));
+
+int ParytiCheck(int[] array)
+{
+    int count = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (array[j] % 2 == 0) count++;
+    }
+    return count;
+}
+
+int[] FillArray(int a)
+{
+    Console.Write("[");
+    int[] arr = new int[a];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+        Console.Write(arr[i] + ",");
+    }
+    Console.Write("] -> ");
+    return arr;
+}
